2c077fe66130fb168a3875ff4cd8b2021b2b3dbc
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-15 12:01:14 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,7 @@ class ApplicationController < ActionController::Base
 
   private
 
-    def authenticate_user
-      redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
-    end
-
+# need to change this?
     def logged_in?
       !!session[:token]
     end
--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -7,4 +7,22 @@ class GithubRepo
     @url = hash["html_url"]
   end
 
+  def authenticate_user
+    client_id = ENV['num']
+    redirect_uri = CGI.escape("http://localhost:3000/auth")
+    github_url = "https://github.com/login/oauth/authorize?client_id=#{num"
+    redirect_to github_url unless logged_in?
+  end
+
+  def authenticate!(client_id, client_secret, code)
+   response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["num"], client_secret: ENV["num"],code: code}, {'Accept' => 'application/json'}
+   access_hash = JSON.parse(response.body)
+
+   user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+   user_json = JSON.parse(user_response.body)
+   session[:username] = user_json["login"]
+
+   redirect_to '/'
+ end
+
 end
