1d7512460d2ab9b45d0475f550633f708eef0f43
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-15 11:39:44 +0000

    Automatically backed up by Learn

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -7,4 +7,19 @@ class GithubRepo
     @url = hash["html_url"]
   end
 
+  def authenticate_user
+    redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['d39518d29d8d39a37017']}&scope=repo" if !logged_in?
+  end
+
+  def authenticate!(client_id, client_secret, code)
+   response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["d39518d29d8d39a37017"], client_secret: ENV["02dbaf073d5a7a127693bdc3c833ca58833ff390"],code: code}, {'Accept' => 'application/json'}
+   access_hash = JSON.parse(response.body)
+
+   user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+   user_json = JSON.parse(user_response.body)
+   session[:username] = user_json["login"]
+
+   redirect_to '/'
+ end
+
 end
